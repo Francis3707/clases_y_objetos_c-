11ee8a267370510349d87f26cdd56d332529b779
Francis3707 <f.vidalpalta2017@gmail.com>
2025-08-22 16:33:34 -0400

    Clases y objetos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Animales
+{
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public void Info()
+    {
+        Console.WriteLine($"bienvenido: {Nombre} {Apellido}");
+    }
+
+}
+public class Program
+{
+    public static void Main()
+    {
+        Animales estudiante1 = new Animales();
+        estudiante1.Nombre = "Francis";
+        estudiante1.Apellido = "Hernandez";
+        estudiante1.Info();
+    }
+}
